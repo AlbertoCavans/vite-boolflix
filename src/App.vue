60f64f4117527d509d182f35f974d0cc95d657da
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-16 16:47:05 +0100

    Feat App.vue - basic structure & link to general.scss

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+@use "./assets/style/general.scss";
+</style>
