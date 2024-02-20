65ef83818c4d566a237e95c096d041907b464ea2
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-20 22:04:11 +0100

    Feat App.vue - print series

--- src/App.vue
+++ src/App.vue
@@ -89,6 +89,22 @@ export default {
   </div>
 
   <div class="mt-4">
+    <h3>Series</h3>
+    <ul v-for="serie in store.series" class="my-3">
+      <li>Titolo: {{ serie.title }}</li>
+      <li>Titolo originale: {{ serie.original_title }}</li>
+      <li>
+        Lingua:
+        <img
+          :src="flagForLanguage(serie.original_language)"
+          alt="flag language"
+          width="30px"
+        />
+      </li>
+      <li>Voto: {{ serie.vote_average }}</li>
+    </ul>
+
+    <h3>Movies</h3>
     <ul v-for="movie in store.movies" class="my-3">
       <li>Titolo: {{ movie.title }}</li>
       <li>Titolo originale: {{ movie.original_title }}</li>
